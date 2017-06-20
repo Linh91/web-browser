d2c9524ecafb8e0320abc235bd9602665ba2f790
Peter Barcsak <peter.barcsak@gmail.com>
2017-06-20 11:10:15 +0100

    Ability to parse a simple HTML line

--- spec/ParserSpec.js
+++ spec/ParserSpec.js
@@ -3,16 +3,22 @@ const Parser = require('../src/Parser.js')
 describe('Parser', function() {
   var htmlString, parser;
 
-  beforeEach(function() {
-    parser = new Parser(htmlString);
-    htmlString = "<p> text </p>"
-  });
 
   it('returns array from html string', function() {
+    htmlString = "<p> text </p>";
+    parser = new Parser(htmlString);
     expect(parser.htmlParser()).toEqual(jasmine.any(Array));
   });
 
   it('returns an array with compenents of p tag', function() {
+    htmlString = "<p> text </p>";
+    parser = new Parser(htmlString);
     expect(parser.htmlParser()).toEqual([["<p>", " text ", "</p>"]]);
+  });
+
+  it('returns an array with compenents of h1 tag', function() {
+    htmlString = "<h1> header </h1>";
+    parser = new Parser(htmlString);
+    expect(parser.htmlParser()).toEqual([["<h1>", " header ", "</h1>"]]);
   });
 });
--- src/Parser.js
+++ src/Parser.js
@@ -1,7 +1,25 @@
-function Parser() {}
+function Parser(htmlString) {
+  this.htmlCharacters = htmlString.split('');
+}
 
 Parser.prototype.htmlParser = function() {
-  return [["<p>", " text ", "</p>"]];
+  var parsedHTML = [], openTag, htmlContent, closeTag, tagArray;
+  openTag = this._getTag();
+  htmlContent = this._getContent();
+  closeTag = this._getTag();
+  tagArray = [openTag, htmlContent, closeTag];
+  parsedHTML.push(tagArray);
+  return parsedHTML;
+};
+
+Parser.prototype._getTag = function () {
+    var endIndex = this.htmlCharacters.indexOf('>');
+    return this.htmlCharacters.splice(0, endIndex + 1).join('');
+};
+
+Parser.prototype._getContent = function () {
+    var endIndex = this.htmlCharacters.indexOf('<');
+    return this.htmlCharacters.splice(0, endIndex).join('');
 };
 
 module.exports = Parser;
