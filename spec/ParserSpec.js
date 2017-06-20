cc4c1af8c4598a1ab80b472af9f916b4170869de
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-20 12:07:21 +0100

    Parses nested tags

--- spec/ParserSpec.js
+++ spec/ParserSpec.js
@@ -10,15 +10,21 @@ describe('Parser', function() {
     expect(parser.htmlParser()).toEqual(jasmine.any(Array));
   });
 
-  it('returns an array with compenents of p tag', function() {
+  it('returns an array with components of p tag', function() {
     htmlString = "<p> text </p>";
     parser = new Parser(htmlString);
     expect(parser.htmlParser()).toEqual([["<p>", " text ", "</p>"]]);
   });
 
-  it('returns an array with compenents of h1 tag', function() {
+  it('returns an array with components of h1 tag', function() {
     htmlString = "<h1> header </h1>";
     parser = new Parser(htmlString);
     expect(parser.htmlParser()).toEqual([["<h1>", " header ", "</h1>"]]);
+  });
+
+  it('returns an array of p tags when h1 tag nested inside', function() {
+    htmlString = "<p><h1> header </h1></p>";
+    parser = new Parser(htmlString);
+    expect(parser.htmlParser()).toEqual([["<p>", ["<h1>", " header ", "</h1>"], "</p>"]]);
   });
 });
--- src/Parser.js
+++ src/Parser.js
@@ -3,23 +3,30 @@ function Parser(htmlString) {
 }
 
 Parser.prototype.htmlParser = function() {
-  var parsedHTML = [], openTag, htmlContent, closeTag, tagArray;
-  openTag = this._getTag();
-  htmlContent = this._getContent();
-  closeTag = this._getTag();
-  tagArray = [openTag, htmlContent, closeTag];
-  parsedHTML.push(tagArray);
+  var parsedHTML = [];
+  parsedHTML.push(this._getLine());
   return parsedHTML;
 };
 
-Parser.prototype._getTag = function () {
+Parser.prototype._getTag = function() {
     var endIndex = this.htmlCharacters.indexOf('>');
     return this.htmlCharacters.splice(0, endIndex + 1).join('');
 };
 
-Parser.prototype._getContent = function () {
+Parser.prototype._getContent = function() {
+    if (this.htmlCharacters[0] === '<') {
+      return this._getLine();
+    }
     var endIndex = this.htmlCharacters.indexOf('<');
     return this.htmlCharacters.splice(0, endIndex).join('');
+};
+
+Parser.prototype._getLine = function() {
+  var openTag, htmlContent, closeTag;
+  openTag = this._getTag();
+  htmlContent = this._getContent();
+  closeTag = this._getTag();
+  return [openTag, htmlContent, closeTag];
 };
 
 module.exports = Parser;
