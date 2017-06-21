74a7b42f6ec0c1d6633b21f1a78688d8576bf99b
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-21 12:05:34 +0100

    Gets rid of jasmine stubbing of console.log and makes renderer return string

--- spec/RendererSpec.js
+++ spec/RendererSpec.js
@@ -5,8 +5,6 @@ describe('Renderer', function() {
 
   it('console logs all text not in tags', function() {
     var parsedHtml = ['<html>', ['<body>', ['<p>', 'Hello world!', '</p>'], '</body>'], '</html>'];
-    console.log = jasmine.createSpy("log");
-    renderer.printContent(parsedHtml);
-    expect(console.log).toHaveBeenCalledWith("Hello world!");
+    expect(renderer.printContent(parsedHtml)).toEqual("Hello world!")
   });
 });
--- src/Renderer.js
+++ src/Renderer.js
@@ -3,10 +3,11 @@ function Renderer(){
 };
 
 Renderer.prototype.printContent = function (parsedHtml) {
+
   if (Array.isArray(parsedHtml[1])) {
-    this.printContent(parsedHtml[1]);
+    return this.printContent(parsedHtml[1]);
   } else {
-    console.log(parsedHtml[1]);
+    return parsedHtml[1]
   };
 };
 
