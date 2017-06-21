69f39a96e2f896968a105fd8d23962db25d0a95f
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-06-21 18:28:37 +0100

    Fixed test on browser spec

--- spec/BrowserSpec.js
+++ spec/BrowserSpec.js
@@ -7,8 +7,10 @@ describe('Browser', function() {
     webUrl = 'web-browser-test.herokuapp.com';
     originalLog = console.log;
     console.log = jasmine.createSpy('log');
-    browser.visitPage(webUrl);
-    expect(console.log).toHaveBeenCalledWith('Hello World');
+    browser.visitPage(webUrl, function(done) {
+      expect(console.log).toHaveBeenCalledWith('Hello World');
+      done();
+    });
     console.log = originalLog;
   });
 });
