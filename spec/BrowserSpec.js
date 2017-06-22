d4d894f638d30815e59902bf72fa6796c328abc7
Peter Barcsak <peter.barcsak@gmail.com>
2017-06-22 18:58:52 +0100

    Browser test still false positive

--- spec/BrowserSpec.js
+++ spec/BrowserSpec.js
@@ -3,17 +3,27 @@ const Browser = require('../src/Browser.js');
 describe('Browser', function() {
   var browser = new Browser();
 
-  it('navigates to a webpage requested by the user', function() {
+  it('navigates to a webpage requested by the user', function(done) {
     webUrl = 'web-browser-test.herokuapp.com';
+    console.log(1)
+    jasmine.clock().install()
+    jasmine.Clock.tick(10)
+    browser.visitPage(webUrl)
     originalLog = console.log;
     console.log = jasmine.createSpy('log');
-    browser.visitPage(webUrl, function(done) {
+      originalLog(3)
       expect(console.log).toHaveBeenCalledWith('Hello world!');
-      expect(console.log).toHaveBeenCalledWith('We are bulding a web browser!');
+      originalLog(4)
+      expect(console.log).toHaveBeenCalledWith('We are building a web browser!');
       expect(console.log).toHaveBeenCalledWith('Platypus');
       expect(console.log).toHaveBeenCalledWith('Header');
+      originalLog(5)
+      console.log = originalLog;
+      value = 0;
       done();
-    });
-    console.log = originalLog;
+    // }, 1000000000000000000000);
+    console.log(6)
+      // done();
+    // });
   });
 });
