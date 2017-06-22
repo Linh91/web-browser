807001d843eb3cd77d92fa30916d791c8ccb55c7
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-22 15:09:50 +0100

    Adds basic command line interface

--- platypus.js
+++ platypus.js
@@ -1,10 +1,12 @@
-function Interface() {
-  this.pic = "                           _.-^~~^^^`~-,_,,~''''''```~,''``~'``~, \n\
+const Browser = require('./src/Browser.js');
+
+function platypus() {
+  const pic = "                           _.-^~~^^^`~-,_,,~''''''```~,''``~'``~, \n\
                    ______,'  -o  :.  _    .          ;     ,'`,  `. \n\
                   (      -\.._,.;;'._ ,(   }        _`_-_,,    `, `, \n\
                    ``~~~~~~'   ((/'((((____/~~~~~~'(,(,___>      `~'"
 
-  this.title =  "                    _____    _           _                                 \n\
+  const title =  "                    _____    _           _                                 \n\
                     |  __ \\  | |         | |                                \n\
                     | |__) | | |   __ _  | |_   _   _   _ __    _   _   ___ \n\
                     |  ___/  | |  / _` | | __| | | | | | '_ \\  | | | | / __|\n\
@@ -12,15 +14,23 @@ function Interface() {
                     |_|      |_|  \\__,_|  \\__|  \\__, | | .__/   \\__,_| |___/\n\
                                                  __/ | | |                  \n\
                                                 |___/  |_|                  "
+
+  var browser = new Browser();
+
+  console.log('\n', pic, '\n', title)
+
+  var readline = require('readline');
+
+  var rl = readline.createInterface(process.stdin, process.stdout);
+  rl.setPrompt('Please enter a url: \n')
+  rl.prompt();
+
+  rl.question("Please enter url: \n", function(webUrl) {
+    browser.visitPage(webUrl);
+    rl.on('line', function(webUrl) {
+      browser.visitPage(webUrl);
+    })
+  });
 };
 
-Interface.prototype.welcome = function () {
-  console.log(this.pic, '\n', this.title)
-  this.browse();
-};
-
-Interface.prototype.browse = function () {
-
-};
-
-module.exports = Interface;
+platypus()
