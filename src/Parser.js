f2593eee646ae605e2c950219dd5eb380b189f87
Peter Barcsak <peter.barcsak@gmail.com>
2017-06-22 14:11:29 +0100

    Parser can parse lines with multiple childs

--- src/Parser.js
+++ src/Parser.js
@@ -2,29 +2,43 @@ function Parser(htmlString) {
   this.htmlCharacters = htmlString.replace(/[\n\r]/g, '').split('');
 }
 
+Parser.prototype._parseNextElement = function() {
+  var nextLine = [];
+  nextLine.push(this._getOpenTag());
+  nextLine.push(this._getContent());
+  nextLine.push(this._getCloseTag());
+  while (Array.isArray(nextLine[nextLine.length - 1])) {
+    nextLine.push(this._getCloseTag());
+  }
+  return nextLine;
+};
+
+Parser.prototype._getCloseTag = function() {
+  if (this._openTag()) {
+    return this._parseNextElement();
+  }
+  var endIndex = this.htmlCharacters.indexOf('>');
+  return this.htmlCharacters.splice(0, endIndex + 1).join('');
+};
 Parser.prototype.parsedHtml = function() {
   return this._parseNextElement();
 };
 
-Parser.prototype._getTag = function() {
+Parser.prototype._getOpenTag = function() {
   var endIndex = this.htmlCharacters.indexOf('>');
   return this.htmlCharacters.splice(0, endIndex + 1).join('');
 };
 
 Parser.prototype._getContent = function() {
-  if (this.htmlCharacters[0] === '<') {
+  if (this._openTag()) {
     return this._parseNextElement();
   }
   var endIndex = this.htmlCharacters.indexOf('<');
   return this.htmlCharacters.splice(0, endIndex).join('');
 };
 
-Parser.prototype._parseNextElement = function() {
-  var openTag, htmlContent, closeTag;
-  openTag = this._getTag();
-  htmlContent = this._getContent();
-  closeTag = this._getTag();
-  return [openTag, htmlContent, closeTag];
+Parser.prototype._openTag = function () {
+  return this.htmlCharacters[0] === '<' && this.htmlCharacters[1] !== '/';
 };
 
 module.exports = Parser;
