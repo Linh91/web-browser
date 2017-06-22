549a9478d05d5fcf2a53ff941087f15284f73139
Peter Barcsak <peter.barcsak@gmail.com>
2017-06-22 16:58:44 +0100

    Renderer can print out lines with multiple content

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,13 +1,15 @@
 function Renderer(){
-
-};
+  this.htmlContent = '';
+}
 
 Renderer.prototype.printContent = function (parsedHtml) {
-  if (Array.isArray(parsedHtml[1])) {
-    return this.printContent(parsedHtml[1]);
-  } else {
-    console.log(parsedHtml[1])
-  };
+  for ( var i = 0; i < parsedHtml.length; i++ ) {
+    if (Array.isArray(parsedHtml[i])) {
+      this.printContent(parsedHtml[i]);
+    } else if ( parsedHtml[i][0] !== '<' ) {
+        console.log(parsedHtml[i]);
+    }
+  }
 };
 
 module.exports = Renderer;
