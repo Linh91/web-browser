5150f179ea3c6b1909bc0c77d03d71155fb54b6c
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-26 11:38:59 +0100

    Fixes multiple keystroke bug. browser now re-renders new websites

--- terminal.js
+++ terminal.js
@@ -83,12 +83,24 @@ var addressBar = blessed.textbox({
   },
 });
 
+// addressBar.on('submit', (text) => {
+//   browser.visitPage(text, function(tag, content) {
+//     addressBar.focus();
+//     screen.render();
+//   });
+//   addressBar.clearValue();
+// })
+
 addressBar.on('submit', (text) => {
-  browser.visitPage(text, function(content) {
+  display.setContent('');
+  browser.visitPage(text, function(tag, content) {
+    if (/<[^>]*a href\s*?/igm.test(tag)) {
+      linksBox.pushLine(content);
+    }
     display.pushLine(content);
-    addressBar.focus();
     screen.render();
   });
+  addressBar.focus();
   addressBar.clearValue();
 })
 
