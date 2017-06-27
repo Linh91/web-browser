ca1acdef8ebccb91ef211a455944f03f569df005
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-27 11:13:28 +0100

    Alters render to associate link tags with nested content

--- src/Renderer.js
+++ src/Renderer.js
@@ -1,31 +1,40 @@
 function Renderer(){
+  this.openATag = false
 }
 
 Renderer.prototype.printContent = function (parsedHtml, fn) {
   for ( var i = 0; i < parsedHtml.length; i++ ) {
     if (Array.isArray(parsedHtml[i])) {
       this.printContent(parsedHtml[i], fn);
+    } else if ( /<[^>]*a\s*?/igm.test(parsedHtml[i]) === true &&
+    (Array.isArray(parsedHtml[i + 1]))) {
+      this.openATag = parsedHtml[i]
     } else if ( parsedHtml[i][0] !== '<' &&
-                /<[^>]*script\s*?/igm.test(parsedHtml[i - 1]) !== true &&
-                /<[^>]*script\s*?/igm.test(parsedHtml[i + 1]) !== true &&
-                /<[^>]*style\s*?/igm.test(parsedHtml[i - 1]) !== true ) {
-      fn(this.convertHtmlChars(parsedHtml[i]), parsedHtml[i-1]);
+    /<[^>]*script\s*?/igm.test(parsedHtml[i - 1]) !== true &&
+    /<[^>]*script\s*?/igm.test(parsedHtml[i + 1]) !== true &&
+    /<[^>]*style\s*?/igm.test(parsedHtml[i - 1]) !== true ) {
+      if(this.openATag === false) {
+        fn(this.convertHtmlChars(parsedHtml[i]), parsedHtml[i-1]);
+      } else {
+        fn(this.convertHtmlChars(parsedHtml[i]), this.openATag);
+        this.openATag = false
+      }
     }
   }
 };
 
 Renderer.prototype.convertHtmlChars = function (htmlText) {
   return htmlText
-    .replace(/&#x21;/g, "!")
-    .replace(/&#x22;/g, "\"")
-    .replace(/&#x23;/g, "#")
-    .replace(/&#x24;/g, "$")
-    .replace(/&#x25;/g, "%")
-    .replace(/&#x26;/g, "&")
-    .replace(/&#x27;/g, "'")
-    .replace(/&#x28;/g, "(")
-    .replace(/&#x29;/g, ")")
-    .replace(/&copy;/g, "©")
-    .replace(/&amp;/g, "&")
+  .replace(/&#x21;/g, "!")
+  .replace(/&#x22;/g, "\"")
+  .replace(/&#x23;/g, "#")
+  .replace(/&#x24;/g, "$")
+  .replace(/&#x25;/g, "%")
+  .replace(/&#x26;/g, "&")
+  .replace(/&#x27;/g, "'")
+  .replace(/&#x28;/g, "(")
+  .replace(/&#x29;/g, ")")
+  .replace(/&copy;/g, "©")
+  .replace(/&amp;/g, "&")
 };
 module.exports = Renderer;
