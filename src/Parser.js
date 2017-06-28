0e5f0c039b92cf8b2d01d793b7d41d4c1b0dc519
Filipe Fasolin <ffasolin@gmail.com>
2017-06-28 12:38:59 +0100

    Changes _parseNextElement to _parse

--- src/Parser.js
+++ src/Parser.js
@@ -1,12 +1,10 @@
-function Parser() {
-  this.response;
-}
+function Parser() {}
 
 Parser.prototype.parseHtml = function (response) {
   this.response = response;
   this._chopPreBody();
   this.htmlCharacters = this._cleanHtml();
-  return this._parseNextElement();
+  return this._parse();
 };
 
 Parser.prototype._chopPreBody = function () {
@@ -22,14 +20,15 @@ Parser.prototype._chopPreBody = function () {
 // removes \n charachters, cleans tabs and returns the array of characters
 Parser.prototype._cleanHtml = function () {
   this.response = this.response.replace(/[\n\r]/g, '');
-  this.response = this.response.split(/>[\s]*</g).join('><');
+  this.response = this.response.split(/>[\s]*</g);
+  this.response = this.response.join('><');
   return this.response.split('');
 };
 
 // Controls the parsing, recursive, and can handle multiple childs in a line with the
 // while loop, which is looping till finds a close tag for the line
 
-Parser.prototype._parseNextElement = function () {
+Parser.prototype._parse = function () {
   var nextLine = [];
   nextLine.push(this._getOpenTag());
   nextLine.push(this._getContent());
@@ -40,28 +39,26 @@ Parser.prototype._parseNextElement = function () {
   return nextLine;
 };
 
-//looking for a close tag, but if it finds an opentag, it starts a new line
-
-Parser.prototype._getCloseTag = function () {
-  if (this._openTag()) {
-    return this._parseNextElement();
-  }
-  var endIndex = this.htmlCharacters.indexOf('>');
-  return this.htmlCharacters.splice(0, endIndex + 1).join('');
-};
-
-
 Parser.prototype._getOpenTag = function () {
   var endIndex = this.htmlCharacters.indexOf('>');
   return this.htmlCharacters.splice(0, endIndex + 1).join('');
 };
 
+//looking for a close tag, but if it finds an opentag, it starts a new line
+
+Parser.prototype._getCloseTag = function () {
+  if (this._openTag()) {
+    return this._parse();
+  }
+  var endIndex = this.htmlCharacters.indexOf('>');
+  return this.htmlCharacters.splice(0, endIndex + 1).join('');
+};
 
 //looking for content, but if it finds an opentag, it starts a new line
 
 Parser.prototype._getContent = function () {
   if (this._openTag()) {
-    return this._parseNextElement();
+    return this._parse();
   }
   var endIndex = this.htmlCharacters.indexOf('<');
   return this.htmlCharacters.splice(0, endIndex).join('');
