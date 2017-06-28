6d155ffc9cf88123d7033422ced2093fe6133ba2
bruxelles86 <w_gant@yahoo.co.uk>
2017-06-28 11:35:03 +0100

    Alters argument names in requester spec

--- spec/RequesterSpec.js
+++ spec/RequesterSpec.js
@@ -1,23 +1,23 @@
 const Requester = require('../src/Requester.js');
 
 describe('Requester', function() {
-  var requester, webUrl;
+  var requester, userInput;
 
   beforeEach(function() {
     requester = new Requester();
   });
 
   it('returns really big pages like the BBC which we presume arrive in several chunks', function(done) {
-      webUrl = 'www.bbc.co.uk';
-      requester.getRequest(webUrl, function(source) {
+      userInput = 'www.bbc.co.uk';
+      requester.getRequest(userInput, function(source) {
         expect(source.length).not.toBeLessThan(200000);
         done();
       });
   }, 10000);
 
   it('returns really big pages which we presume arrive in several chunks', function(done) {
-    webUrl = 'web-browser-test.herokuapp.com';
-    requester.getRequest(webUrl, function(source) {
+    userInput = 'web-browser-test.herokuapp.com';
+    requester.getRequest(userInput, function(source) {
     expect(source).toEqual('<html>\n' +
                            '<body>\n' +
                            '<h1>Header</h1>\n' +
@@ -31,16 +31,16 @@ describe('Requester', function() {
   }, 10000);
 
   it('makes requests to different paths', function(done) {
-      webUrl = 'www.bbc.co.uk/news';
-      requester.getRequest(webUrl, function(source) {
+      userInput = 'www.bbc.co.uk/news';
+      requester.getRequest(userInput, function(source) {
         expect(source).toContain('Scotland', 'N. Ireland', 'Business');
         done();
       });
   }, 10000);
 
   it('makes requests to different paths', function(done) {
-      webUrl = 'web-browser-test.herokuapp.com/testpath';
-      requester.getRequest(webUrl, function(source) {
+      userInput = 'web-browser-test.herokuapp.com/testpath';
+      requester.getRequest(userInput, function(source) {
         expect(source).toContain("The platypus is among nature's most unlikely animals.");
         done();
       });
